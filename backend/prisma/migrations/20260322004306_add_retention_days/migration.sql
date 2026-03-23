f55309934160384b218f35ee0bef13b4b8ea35d1
eaguilar <67742623+Elias-Aguilar@users.noreply.github.com>
2026-03-23 08:31:42 -0300

    Revert "firt commit"
    This reverts commit cdb83acf62937a9406aff952b35e4e15b69ac146.

--- backend/prisma/migrations/20260322004306_add_retention_days/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Camera" ADD COLUMN     "retentionDays" INTEGER;
--- backend/src/types/onvif.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "onvif" {
-  // Tipado mínimo para poder compilar; se puede refinar más adelante.
-  export namespace Discovery {
-    function probe(
-      callback: (err: unknown, cams: any[]) => void
-    ): void;
-  }
-}
-
